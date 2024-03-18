4b566244912ded62bf8936bd62186948edfbc44c
Jooyung Han <jooyung@google.com>
2024-03-18 14:23:49 +0900

    X6739: Install apex for clearkey service
    Bug: 329737888
    Test: local build
    Change-Id: Ibf5574ce7da0b927ef8179663bb03a039955bcd3
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -137,7 +137,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey
+    com.android.hardware.drm.clearkey
 
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
