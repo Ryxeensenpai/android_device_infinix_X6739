4945a238a1835fbc41ea1e760691c92bd8294ee0
fjrXTR <fajarslebew31@gmail.com>
2025-10-06 15:08:18 +0200

    X6739: Add `hw/transsion` on soong namespaces
    Change-Id: I85f9dee6438655781c7f4864f41135ef442d589b
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -410,7 +410,8 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/mediatek \
-    hardware/mediatek/libmtkperf_client
+    hardware/mediatek/libmtkperf_client \
+    hardware/transsion
 
 # Thermal
 PRODUCT_PACKAGES += \
