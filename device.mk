e11fafb56d66b3e45860a823ece12c56f1b59ab3
fjrXTR <fajarslebew31@gmail.com>
2025-10-06 14:54:19 +0200

    X6739: Switch to AOSP bluetooth services
    Change-Id: I3591d0066a5875e9a65e23ea5c297e1ae8acc504
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -94,8 +94,11 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.1.vendor \
-    android.hardware.bluetooth.audio-impl
+    android.hardware.bluetooth@1.1.vendor:64
+
+PRODUCT_PACKAGES += \
+    vendor.mediatek.hardware.bluetooth.audio@2.1.vendor:64 \
+    vendor.mediatek.hardware.bluetooth.audio@2.2.vendor:64
 
 # Boot Control
 PRODUCT_PACKAGES += \
