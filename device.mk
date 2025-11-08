48ace624ac1e4032690aacd71075fccfa69dcc5d
fjrXTR <fajarslebew31@gmail.com>
2025-11-08 13:19:51 +0100

    X6739: Drop Secure element * it's useless on MTK devices
    Change-Id: Ic18cf26644b8e005391dc46f9e0f712011d0b34d
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -375,10 +375,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/init.recovery.mt6893.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.mt6893.rc
 
-# Secure Element
-PRODUCT_PACKAGES += \
-    android.hardware.secure_element@1.2.vendor
-
 # Sensors
 PRODUCT_PACKAGES += \
     libsensorndkbridge
