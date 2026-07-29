4020abaa49e5a7bb8f50c914265d29fc055191b1
vlxlxlv <fajarslebew31@gmail.com>
2026-07-29 06:26:49 +0200

    X6739: Switch to Stock USB HAL services
    Change-Id: I20dcb6dea71d22607e8bd3e9880607ffe1fcb856
    Signed-off-by: vlxlxlv <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -421,13 +421,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@2.0.vendor
 
-# USB
-$(call soong_config_set_bool,android_hardware_mediatek_usb,audio_accessory_supported,true)
-
-PRODUCT_PACKAGES += \
-    android.hardware.usb-service.mediatek \
-    android.hardware.usb.gadget-service.mediatek
-
 # Vibrator
 VIBRATOR_SUPPORTS_EFFECTS := true
 
